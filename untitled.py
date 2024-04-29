4e01a2ef531c25ce83f7d2e9ec7ce35b749469d1
Pierre de Thoisy <pierre.dethoisy@gitguardian.com>
2024-04-29 13:50:46 +0200

    Update untitled.py

--- untitled.py
+++ untitled.py
@@ -13,3 +13,5 @@ leCurseur.query("update books set title='assommoir' where author='Zola'")
 uneConnexionBDD.commit()
 
 pwd= sdFGf3-rereh444
+
+apikey='xoxb-163213206324-zSDGSFDGh1242312435F12344ZR2324532554641ZEZ134235R',
